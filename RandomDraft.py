c7182afd2eec7c2d3fa41884d84ca0c3c4615e7a
Ronnie Perez <RonnieCodes95@gmail.com>
2024-08-28 15:40:38 -0500

    Changed range to n to make it adjustable to the list total

--- RandomDraft.py
+++ RandomDraft.py
@@ -1,9 +1,10 @@
 import random
 
-UserRoster = ['Ronnie', 'Carmen', 'Eric', 'Amelia', 'Ethan', 'Erics Mom']
+UserRoster = ['Ronnie', 'Carmen', 'Eric', 'Amelia', 'Ethan', 'Erics Mom', 'Eddie', 'Danny']
 RandomizedRoster = []
+n = len(UserRoster)
 
-for users in range(6):
+for users in range(n):
     random_choice = random.choice(UserRoster)
     RandomizedRoster.append(random_choice)
     UserRoster.remove(random_choice)
